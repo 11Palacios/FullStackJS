969c734a05026757f2025e4359ac8b92c255f2a4
Jose Luis Palacios <hola@jlpalacios.es>
2022-07-09 18:10:17 +0200

    morgan middleware added

--- E3.1-3.6/index.js
+++ E3.1-3.6/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
+app.use(morgan('combined'))
 let persons = [
     {
         id: 1,
@@ -30,10 +31,12 @@ app.get('/info', (request, response) => {
     const length = persons.length
     const date = new Date()
     response.send('<div><p>Phonebook has info for ' + length + ' people</p><p>' + date + '</p></div>')
+    morgan('tiny')
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+    morgan('tiny')
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -41,8 +44,10 @@ app.get('/api/persons/:id', (request, response) => {
     const person = persons.filter(person => person.id === id)
     if(person){
         response.json(person)
+        morgan('tiny')
     }else{
         response.status(404).end()
+        morgan('tiny')
     }
 })
 
@@ -50,6 +55,7 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+    morgan('tiny')
 })
 
 const randomId = (max) => {
@@ -61,16 +67,17 @@ app.post('/api/persons/', (request, response) => {
     if(!body.name){
         return response.status(400).json({
             error: 'name missing'
-        })}
+        }, morgan('tiny'))
+    }
     if(!body.number){
         return response.status(400).json({
             error: 'number missing'
-    })}
+    }, morgan('tiny') )}
     const nom = persons.find(person => person.name === body.name)
     if(nom !== undefined){
         return response.status(400).json({
             error: 'Name must be unique'
-        })
+        }),morgan('tiny')
     }
 
 
@@ -83,11 +90,13 @@ app.post('/api/persons/', (request, response) => {
     persons = persons.concat(person)
 
    response.json(person)
+   morgan('dev')
 
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
+    morgan('tiny')
 }
 
 app.use(unknownEndpoint)
